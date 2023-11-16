327206e83a34d6b6f787c62756bc0ca9a4f14f27
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-11-16 11:02:37 +0100

    correction

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,6 +1,25 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
 // stampa un messaggio appropriato sull’esito del controllo.
 
+const vipList = ['viktor', 'marco', 'mirko', 'silvia']
+console.log(vipList)
+
+let userName = prompt('Type your name please')
+
+if(vipList.includes(userName)) {
+    console.log('You can enter')
+}
+else {
+    console.log('Sorry, you are not on the list')
+}
+
+for (let i = 0; i < vipList.length; i++){
+    const currentMail = vipList [i];
+    console.log(i, currentMail);
+}
+
+// Correzione
+
 const mailingList = [
     'havanaclub@gmail.com',
     'vogliadimare@gmail.com',
